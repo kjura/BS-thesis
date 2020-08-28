8404bea87bbb2c16801faf90eecda243d8a0f0e5
kjura <kjura@users.noreply.github.com>
2020-08-28 14:25:54 +0200

    Upload chapter 2 from Rashka
    Still handling the issue why do we have to update all of the weights in one iteration.

--- rashka_chp2/ch02.py
+++ rashka_chp2/ch02.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 class Pyceptron():
-    '''
+    """
     My own implementation of a perceptron classifier.
 
     Parameters
@@ -14,25 +14,34 @@ class Pyceptron():
         Maximum number of passes over the training dataset
     model_seed : int
         Seed for generating random weight initialization
-    '''
+    """
     def __init__(self, eta, epoch=50, model_seed=1):
         self.eta = eta
         self.epoch = epoch
         self.model_seed = model_seed
 
     def fit(self, X, y):
-        '''
+        """
         Method that fits the model given the X-elements
-        to a y
+        to y.
         :param X: Data for training
         :param y: Target values
         :return: self
-        '''
+        """
 
         self.error = []
         # Call for Mersenne Twister pseudo-random number generator
         pseudo_generator = np.random.RandomState(self.model_seed)
-        self.w_j = pseudo_generator.normal(spread=0.1, size=1 + X.shape[1])
+        self.w_j = pseudo_generator.normal(loc=0.0, scale=0.1, size=1 + X.shape[1])
+
+        for epochs in range(self.epoch):
+            error_measure = 0
+            for training_example, true_class_lbl in zip(X, y):
+                delta_w_j = self.eta * (true_class_lbl - self.threshold_fun(training_example))
+                self.w_j[0] += delta_w_j
+                self.w_j[1:] += delta_w_j * training_example
+
+
 
 
     def net_inp_fun(self, X):
